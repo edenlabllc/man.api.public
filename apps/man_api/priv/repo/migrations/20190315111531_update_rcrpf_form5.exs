4998e273e562a3df804956d32bd7f302a46fa620
Denis Klimenko <denis.klimenko.dx@gmail.com>
2019-03-15 13:23:22 +0200

    chore: update reimbursement contract template (#31)

---
+++ apps/man_api/priv/repo/migrations/20190315111531_update_rcrpf_form5.exs
@@ -0,0 +1,9 @@
+defmodule Man.Repo.Migrations.UpdateRcrpfForm5 do
+  use Ecto.Migration
+  
+  alias Man.Templates.API
+  
+  def change do
+    API.update_template_body!("RCRPF", Application.app_dir(:man_api, "priv/static/RCRPF.html.eex"))
+  end
+end
