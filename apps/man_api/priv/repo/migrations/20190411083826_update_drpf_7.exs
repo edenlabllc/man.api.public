b8bd94dde5122e564cc03f2532b46b1fd3d655ba
Denis Klimenko <denis.klimenko.dx@gmail.com>
2019-04-11 11:50:13 +0300

    fix: update drpf template (#41)

---
+++ apps/man_api/priv/repo/migrations/20190411083826_update_drpf_7.exs
@@ -0,0 +1,9 @@
+defmodule Man.Repo.Migrations.UpdateDrpf7 do
+  use Ecto.Migration
+
+  alias Man.Templates.API
+
+  def change do
+    API.update_template_body!("DRPF", Application.app_dir(:man_api, "priv/static/DRPF.html.eex"))
+  end
+end
